a2a4e49cfab7bdedafc6943104f820f12ed47e3f
Abdullah Ghazi <aghazi@lab3r7s5.42abudhabi.ae>
2022-08-30 17:26:42 +0400

    stop infint

--- philo.c
+++ philo.c
@@ -6,26 +6,123 @@
 /*   By: aghazi <aghazi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 17:10:22 by aghazi            #+#    #+#             */
-/*   Updated: 2022/08/23 18:59:14 by aghazi           ###   ########.fr       */
+/*   Updated: 2022/08/30 17:24:48 by aghazi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "philo.h"
 
+int take_forks(t_philo *philo)
+{
+    pthread_mutex_lock(philo->right_mutex);
+    if (*(philo->right_fork) == 0)
+    {
+        *(philo->right_fork) = 1;
+        printf("phlio no %d picked right fork \n", philo->philo_id);
+        pthread_mutex_unlock(philo->right_mutex);
+        pthread_mutex_lock(philo->left_mutex);
+        if (*(philo->left_fork) == 0)
+        {
+            *(philo->left_fork) = 1;
+            printf("phlio no %d picked LEFT fork \n", philo->philo_id);
+            pthread_mutex_unlock(philo->left_mutex);
+            return (1);
+        }
+        else
+        {
+            pthread_mutex_unlock(philo->left_mutex);
+            pthread_mutex_lock(philo->right_mutex);
+            *(philo->right_fork) = 0;
+            printf("phlio no %d place back right fork \n", philo->philo_id);
+        }
+    }
+    pthread_mutex_unlock(philo->right_mutex);
+    return (0);
+}
 
 void    *start(void *philo)
 {
     
     t_philo *ph = (t_philo *)philo;
-    
-    //lock right fork && check fork value
-    //if free pick fork.
-    //lock left fork && check fork value
-    //if free pick fork.
+    int i;
 
+    i = 0;
+    while (i < ph->general_info->no_of_philos)
+    {
+        while (take_forks(ph))
+        {
+            usleep(500);
+        }
+        i++;
+    }
+    
+    
     
     return NULL;
+}
+
+int init_mutex(t_philo *ph, t_info *info)
+{
+    int i;
+
+    i = 0;
+
+    while(i < info->no_of_philos)
+    {
+        if (pthread_mutex_init(&info->mutex[i], NULL) != 0)
+            return (1);
+        i++;
+    }
+    i = 0;
+    while (i < info->no_of_philos)
+    {
+        ph[i].right_mutex = &info->mutex[i];
+        if (i == 0)
+            ph[i].left_mutex = &info->mutex[info->no_of_philos - 1];
+        else
+            ph[i].left_mutex = &info->mutex[i];
+        i++;
+    }
+    return (0);
+}
+
+int init_forks(t_philo *ph, t_info *info)
+{
+    int i;
+
+    i = 0;
+    info->forks = calloc(info->no_of_philos,sizeof(int));
+    while (i < info->no_of_philos)
+    {
+        ph[i].right_fork = &info->forks[i];
+        if (i == 0)
+            ph[i].left_fork = &info->forks[info->no_of_philos - 1];
+        else
+            ph[i].left_fork = &info->forks[i];
+        i++;
+    }
+    return (0);
+}
+
+int init_philos(t_philo *ph, t_info *info)
+{
+    if (init_mutex(ph, info))
+        return (1);
+    if (init_forks(ph, info))
+        return (1);
+    return (0);
+}
+
+int parse_input(int ac, char **av, t_info *info)
+{
+    info->no_of_philos = atoi(av[1]);
+    info->time_to_die = atoi(av[2]);
+    info->time_to_eat = atoi(av[3]);
+    info->time_to_sleep = atoi(av[4]);
+
+    if(ac == 6)
+        info->no_of_times_eat = atoi(av[5]);
+    return (0);
 }
 
 int main(int arc, char **argv)
@@ -33,19 +130,18 @@ int main(int arc, char **argv)
     t_philo philos[200];
     t_info  info;
     int i;
-    info.death_flag = 0;
+    
     i = 0;
+    philos->right_fork = 0;
+    philos->left_fork = 0;
+    
     pthread_t life_thread;
-    pthread_mutex_init(&info.mutex1, NULL);
     if (arc == 5 || arc == 6)
     {
-        info.no_of_philos = atoi(argv[1]);
-        info.time_to_die = atoi(argv[2]);
-        info.time_to_eat = atoi(argv[3]);
-        info.time_to_sleep = atoi(argv[4]);
-        
-        if(arc == 6)
-            info.no_of_times_eat = atoi(argv[5]);
+        if (parse_input(arc, argv, &info))
+            return (1);
+        if (init_philos(philos, &info))
+            return (1);
         while (i < info.no_of_philos)
         {
             philos[i].general_info = &info;
@@ -58,7 +154,7 @@ int main(int arc, char **argv)
             pthread_join(life_thread, NULL);
             info.no_of_philos--;
         }
-        printf("flag = %d \n", info.death_flag);
+        // printf("flag = %d \n", info.death_flag);
     }
     return (0);
 }
--- philo.h
+++ philo.h
@@ -7,30 +7,27 @@
 #include <pthread.h>
 
 typedef struct s_info{
-
     int no_of_philos;
+    int *forks;
     int time_to_die;
     int time_to_eat;
     int time_to_sleep;
     int no_of_times_eat;
     int death_flag;
-    pthread_mutex_t mutex[200];// 5
-
+    pthread_mutex_t mutex[200];
 }t_info;
 
 
 typedef struct s_philo{
-
     int start_eat;
     int philo_id;
     t_info *general_info;
-    int right_fork;
-    int left_fork;
+    int *right_fork;
+    int *left_fork;
     pthread_mutex_t *right_mutex;
     pthread_mutex_t *left_mutex;
-
 }t_philo;
 
 
 
-#endif // PHILO_H
+#endif
